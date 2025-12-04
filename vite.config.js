9fdb5813e4a7200924414c25360b68279df60545
Riki Miyata <m31riki@gmail.com>
2025-12-04 22:14:48 +0900

    修正: PWAアイコンのpurpose設定を追加

--- vite.config.js
+++ vite.config.js
@@ -21,12 +21,14 @@ export default defineConfig({
           {
             src: 'images/favicon.png', // あなたが用意したアイコン
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: 'images/favicon.png', // 大きいサイズも同じ画像を指定しておきます
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
