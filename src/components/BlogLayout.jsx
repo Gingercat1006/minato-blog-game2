0c86a808e8715f181986ff56042f8548f780f85c
Riki Miyata <m31riki@gmail.com>
2025-12-10 00:16:38 +0900

    コメント修正

--- src/components/BlogLayout.jsx
+++ src/components/BlogLayout.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Sidebar from './Sidebar';
-// GameContextから直接データをもらってもいいですが、
-// 今はSidebarやOutletの中身が自分でデータを取りに行くので、ここは単純な枠組みだけでOKです。
+
 
 const BlogLayout = () => {
   return (
@@ -14,7 +13,7 @@ const BlogLayout = () => {
         <main id="content">
           <Outlet />
         </main>
-        <Sidebar /> {/* propsを渡す必要なし！ */}
+        <Sidebar /> 
       </div>
       <footer id="footer">
         <p>Copyright &copy; Minato's Room. All Rights Reserved.</p>
